80626611cfae77f047c92e4afd75a990763a9844
PursuitYP <1844507435@qq.com>
2023-06-26 21:02:31 +0800

    update

--- chatbot2.py
+++ chatbot2.py
@@ -16,6 +16,7 @@ from langchain.agents import initialize_agent
 from llama_index.langchain_helpers.agents import LlamaToolkit, create_llama_chat_agent, IndexToolConfig, GraphToolConfig
 from llama_index.indices.query.query_transform.base import DecomposeQueryTransform
 
+
 load_dotenv()
 
 os.environ["http_proxy"] = "http://10.10.1.3:10000"
