f39bbe6f8381f206f778534af3cda5fd6a2b7ae7
PursuitYP <1844507435@qq.com>
2023-07-11 10:31:31 +0800

    pack into a def function

--- mgkg_relation.py
+++ mgkg_relation.py
@@ -13,15 +13,6 @@ import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import logging
 
-load_dotenv()
-
-os.environ["http_proxy"] = "http://10.10.1.3:10000"
-os.environ["https_proxy"] = "http://10.10.1.3:10000"
-
-# Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
-os.environ['OPENAI_API_KEY'] = openai.api_key
-
 
 # Use gpt-3.5-turbo with designed prompt to extract data
 def extract_chunk(document, template_prompt):
@@ -44,7 +35,7 @@ def extract_chunk(document, template_prompt):
 
 # 调用API并使用重试机制处理rate limit error和其他异常
 def get_completion(prompt, model="gpt-3.5-turbo"):
-    for i in range(3):  # Retry the API call up to 3 times
+    for i in range(5):  # Retry the API call up to 5 times
         try:
             messages = [{"role": "user", "content": prompt}]
             response = openai.ChatCompletion.create(
@@ -169,11 +160,7 @@ def relation_alignment(sentence, head, tail, relation, schema_relations):
     relation_aligned_example_1 = "caution"
 
     triple = [head, relation, tail]
-    prompt_prefix_copy = f'''You will read a "sentence" and a "triple" \[head entity, "relation", tail entity\] extracted from that sentence.
-Please align the "relation" into one of the following "categories" based on the contextual information of this relation in the sentence.
-If you think the relation does not belong to any of the following categories, output \"none\".
-Your output can only be one of these categories or \"none\".
-'''
+
     relation_prompt = f'''You will read a 'sentence' and a 'triple' (\[head entity, 'relation', tail entity\]) extracted from that sentence.
 Please align this "relation" into one of the following "relation schemas" based on the contextual information of that relation in the sentence and in the triple.
 If you believe that the relation cannot be aligned to any of the following relation schemas, output \"none\".
@@ -237,22 +224,13 @@ def process_relations(sentence, relations, entity_labels, schema_relations):
         tail_label = entity_linking(sentence, tail, entity_labels)
         # 如果head_label或者tail_label为none，直接continue
         if head_label == "none" or tail_label == "none":
-            # print("@ jump 1")
             continue
-        # print(head, " @ ", relation, " @ ", tail)
-        # print(head, " @ ", head_label)
-        # print(tail, " @ ", tail_label)
-        # print("-" * 50)
 
         # 2. 根据schema_relations进行关系对齐
         relation_aligned = relation_alignment(sentence, head, tail, relation, schema_relations)
         # 如果relation_aligned为none，直接continue
         if relation_aligned == "none":
-            # print("@ jump 2")
             continue
-        # print(head, " @ ", relation, " @ ", tail)
-        # print(head, " @ ", relation_aligned, " @ ", tail)
-        # print("-" * 100)
 
         # 3. 设置筛选条件，更新new_relations
         new_relation = [head, head_label, relation_aligned, relation, tail, tail_label]
@@ -261,112 +239,135 @@ def process_relations(sentence, relations, entity_labels, schema_relations):
     return new_relations
 
 
-# 打开并读取stopwords
-with open('stopwords.txt', 'r') as f:
-    stopwords_f = f.readlines()
-stopwords_ls = []
-for w in stopwords_f:
-    stopwords_ls.append(w.strip().strip('\n'))
-
-
-# 获取并打印entity_labels和schema_relations
-entity_labels = get_entity_labels()
-schema_relations = get_relations()
-print("# entity labels and schema relations: ")
-print(len(entity_labels), entity_labels)
-print(len(schema_relations), schema_relations)
-print("-" * 100)
-
-
-# rel_pubmed_0508, rel_selected_papers, rel_clinical_trials
-# rel_1_10001, rel_230001_240001, rel_380001_390001 rel_490001_500001
-# rel_file = "../../projects/openie6/rel_results/rel_pubmed_0508.txt"
-rel_file = "../../projects/openie6/predictions.txt"
-# 新关系结构：[head, head_label, relation_aligned, old_relation, tail, tail_label]
-final_relations = []    # 处理后最终的关系：三元组清洗、实体链接、关系对齐
-sentence_relations_pack = []    # 保存每个句子和从中抽取到的多个关系
-print("# new relation schema: [head, head_label, relation_aligned, old_relation, tail, tail_label]")
-# 打开待处理的rel_file，读取数据，进行实体链接和关系对齐
-with open(rel_file, 'r') as f:
-    lines = f.readlines()
-    cnt = 0
-    sentence = ""
-    relations = []
-    for line in tqdm(lines, desc='Processing sentence lines'):
-        # 移除句子结尾的 "\n"
-        line = line[:-1]
-        cnt += 1
-        # if cnt <= 1000:
-        if True:
-            # print(line)
-            # 如果遇到空行，说明一个句子和关系结束了
-            if line == "":
-                # 打印输出，跳过没有抽取到关系的句子
-                if relations != []:
-                    sentence_relations_item = [sentence, relations]
-                    sentence_relations_pack.append(sentence_relations_item)
-                    # """ 在这里根据sentences对relations进行处理, 如实体链接和关系对齐 """
-                    # new_relations = process_relations(sentence, relations, entity_labels, schema_relations)
-                    # # 新关系结构：[head, head_label, relation_aligned, 0ld_relation, tail, tail_label]
-                    # for item in new_relations:
-                    #     # 边处理边写入，写入处理好的新关系
-                    #     writer.writerow(item)
-                    #     final_relations.append(item)
-                    # print(sentence)
-                    # # print(relations)
-                    # for relation_item in relations:
-                    #     print(relation_item)
-                    # print()
-                # 重置sentence和relations
-                sentence = ""
-                relations = []
-            # 否则，说明该行是句子或者抽取的关系
-            else:
-                # 如果sentence为空，说明该句是sentence
-                if sentence == "":
-                    sentence = line
-                # 否则，该句为关系，提取 [confidence, head, relation, tail] ，加到relations
+# 从txt文件中读取关系，并进行预处理筛除一些项
+def get_relations_from_txt(input_rel_file):
+    sentence_relations_pack = []    # 保存每个句子和从中抽取到的多个关系
+    print("# new relation schema: [head, head_label, relation_aligned, old_relation, tail, tail_label]")
+    # 打开待处理的rel_file，读取数据，进行实体链接和关系对齐
+    with open(input_rel_file, 'r') as f:
+        lines = f.readlines()
+        cnt = 0
+        sentence = ""
+        relations = []
+        for line in tqdm(lines, desc='Processing sentence lines'):
+            # 移除句子结尾的 "\n"
+            line = line[:-1]
+            cnt += 1
+            # if cnt <= 1000:
+            if True:
+                # print(line)
+                # 如果遇到空行，说明一个句子和关系结束了
+                if line == "":
+                    # 打印输出，跳过没有抽取到关系的句子
+                    if relations != []:
+                        sentence_relations_item = [sentence, relations]
+                        sentence_relations_pack.append(sentence_relations_item)
+                    # 重置sentence和relations
+                    sentence = ""
+                    relations = []
+                # 否则，说明该行是句子或者抽取的关系
                 else:
-                    confidence = float(line[0:4])
-                    relation = re.findall(";.*;", line)[0].strip(';').strip(';').strip()
-                    head = re.findall("\(.*;", line)[0].strip('\(').strip(';').strip()
-                    head = head.replace(relation, "")[:-2]
-                    tail = re.findall(";.*\)", line)[0].strip(';').strip('\)').strip()
-                    tail = tail.replace(relation, "")[2:]
-                    # 三元组不完整，跳过
-                    if head == "" or relation == "" or tail == "":
-                        continue
-                    # 三元组中存在stopwords，跳过
-                    if head.lower() in stopwords_ls or relation.lower() in stopwords_ls or tail.lower() in stopwords_ls:
-                        continue
-                    # 实体太长（>6），跳过
-                    if len(head.split()) > 6 or len(tail.split()) > 6:
-                        continue
-                    # confidence分数小于0.6，跳过
-                    if confidence < 0.6:
-                        continue
-                    relation_info = [confidence, head, relation, tail]
-                    relations.append(relation_info)
+                    # 如果sentence为空，说明该句是sentence
+                    if sentence == "":
+                        sentence = line
+                    # 否则，该句为关系，提取 [confidence, head, relation, tail] ，加到relations
+                    else:
+                        confidence = float(line[0:4])
+                        relation = re.findall(";.*;", line)[0].strip(';').strip(';').strip()
+                        head = re.findall("\(.*;", line)[0].strip('\(').strip(';').strip()
+                        head = head.replace(relation, "")[:-2]
+                        tail = re.findall(";.*\)", line)[0].strip(';').strip('\)').strip()
+                        tail = tail.replace(relation, "")[2:]
+                        # 三元组不完整，跳过
+                        if head == "" or relation == "" or tail == "":
+                            continue
+                        # 三元组中存在stopwords，跳过
+                        if head.lower() in stopwords_ls or relation.lower() in stopwords_ls or tail.lower() in stopwords_ls:
+                            continue
+                        # 实体太长（>6），跳过
+                        if len(head.split()) > 6 or len(tail.split()) > 6:
+                            continue
+                        # confidence分数小于0.6，跳过
+                        if confidence < 0.6:
+                            continue
+                        # 如果动物实验相关名词在实体名中出现，跳过
+                        animal_flag = False
+                        for key in exp_animals_keys:
+                            if key.lower() in head.lower() or key.lower() in tail.lower():
+                                animal_flag = True
+                                break
+                        if animal_flag:
+                            continue
+                        relation_info = [confidence, head, relation, tail]
+                        relations.append(relation_info)
+                        
+    return sentence_relations_pack
 
 
-# write_csv_file = "../../projects/openie6/rel_results/rel_pubmed_0508_new.csv"
-write_csv_file = "../../projects/openie6/predictions_new.txt"
-log_file = 'log_mgkg.txt'
-with open(write_csv_file, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    # 写入csv表头
-    writer.writerow(["head", "head_label", "relation", "old_relation", "tail", "tail_label"])
-    with ThreadPoolExecutor() as executor:
-        # 多线程处理
-        futures = {executor.submit(process_relations, item[0], item[1], entity_labels, schema_relations): item for item in sentence_relations_pack}
-        for future in tqdm(as_completed(futures), total=len(futures), desc='Processing relations'):
-            # 收集完成的线程处理好的结果
-            new_relations = future.result()
-            if new_relations is None:
-                log_to_file(log_file, f'Failed to process item {futures[future]}')
-            else:
-                for item in new_relations:
-                    # 边处理边写入，写入处理好的新关系
-                    writer.writerow(item)
-                    final_relations.append(item)
-                log_to_file(log_file, f'Successfully processed item!')
+def main(input_rel_file, write_csv_file):
+    # 从txt文件中读取关系，并进行预处理筛除一些项
+    sentence_relations_pack = get_relations_from_txt(input_rel_file)
+
+    # 新关系结构：[head, head_label, relation_aligned, old_relation, tail, tail_label]
+    final_relations = []    # 处理后最终的关系：三元组清洗、实体链接、关系对齐
+    with open(write_csv_file, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        # 写入csv表头
+        writer.writerow(["head", "head_label", "relation", "old_relation", "tail", "tail_label"])
+        # 限制最大线程数为4，缓解rate_limit_exceeded报错
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            # 多线程处理
+            futures = {executor.submit(process_relations, item[0], item[1], entity_labels, schema_relations): item for item in sentence_relations_pack}
+            for future in tqdm(as_completed(futures), total=len(futures), desc='Processing relations'):
+                # 收集完成的线程处理好的结果
+                new_relations = future.result()
+                if new_relations is None:
+                    log_to_file(log_file, f'Failed to process item {futures[future]}')
+                else:
+                    for item in new_relations:
+                        # 边处理边写入，写入处理好的新关系
+                        writer.writerow(item)
+                        final_relations.append(item)
+                    log_to_file(log_file, f'Successfully processed item!')
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    os.environ["http_proxy"] = "http://10.10.1.3:10000"
+    os.environ["https_proxy"] = "http://10.10.1.3:10000"
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    os.environ['OPENAI_API_KEY'] = openai.api_key
+
+    # 打开并读取stopwords
+    with open('stopwords.txt', 'r') as f:
+        stopwords_f = f.readlines()
+    stopwords_ls = []
+    for w in stopwords_f:
+        stopwords_ls.append(w.strip().strip('\n'))
+
+    # 获取并打印entity_labels和schema_relations
+    entity_labels = get_entity_labels()
+    schema_relations = get_relations()
+    print("# entity labels and schema relations: ")
+    print(len(entity_labels), entity_labels)
+    print(len(schema_relations), schema_relations)
+    print("-" * 100)
+
+    # 动物实验关键词和指代不明常见词
+    exp_animals_keys = ['experimental autoimmune myasthenia gravis', 'EAMG', 'dog', 'mice', 'animal', 'guinea pig', 'rats', 'vertebrate']
+    ref_ubclear_keys = ['patient', 'medication', 'drug', 'test', 'gene', 'symptom', 'scale']
+
+    # input_rel_file = "../../projects/openie6/predictions.txt"
+    # write_csv_file = "../../projects/openie6/predictions_new.csv"
+    # input_rel_file = "../../projects/openie6/rel_results/rel_abs_pubmed_selected_clinical.txt"
+    # write_csv_file = "../../projects/openie6/rel_results/rel_abs_pubmed_selected_clinical_new.csv"
+    # input_rel_file = "../../projects/openie6/rel_results/rel_abs_1_10001.txt"
+    # write_csv_file = "../../projects/openie6/rel_results/rel_abs_1_10001_new.csv"
+    input_rel_file = "../../projects/openie6/rel_results/rel_abs_90001_100001.txt"
+    write_csv_file = "../../projects/openie6/rel_results/rel_abs_90001_100001_new.csv"
+    print(f"input_rel_file: {input_rel_file}")
+    print(f"write_csv_file: {write_csv_file}")
+    print("-" * 100)
+    log_file = 'log_mgkg.txt'
+
+    main(input_rel_file, write_csv_file)
